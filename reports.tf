aa08bbea2a4c287bc8be642b112684b826fc9769
John Favorite <john.favorite@actioniq.com>
2022-03-01 15:28:52 -0500

    tweaks

--- reports.tf
+++ reports.tf
@@ -1,3 +1,23 @@
+
+# DuckbillGroupBilling AWS Cost and Usage Reports
+
+resource "aws_cur_report_definition" "DuckbillGroup_cur_report" {
+  count                      = var.duckbillgroup_cur_report_create ? 1 : 0
+  report_name                = var.duckbillgroup_cur_report_name
+  time_unit                  = "HOURLY"
+  format                     = "Parquet"
+  compression                = "Parquet"
+  additional_schema_elements = ["RESOURCES"]
+  s3_bucket                  = var.cur_bucket_name
+  s3_prefix                  = "cur"
+  s3_region                  = "us-east-1"
+  additional_artifacts       = ["ATHENA"]
+  report_versioning          = "OVERWRITE_REPORT"
+}
+
+
+# DuckbillGroupBilling S3 Bucket and Policy - AWS Cost and Usage Reports
+
 resource "aws_s3_bucket" "cost_and_usage_bucket" {
   count  = var.cur_bucket_name_create ? 1 : 0
   bucket = var.cur_bucket_name
--- variables.tf
+++ variables.tf
@@ -3,23 +3,24 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "duckbillgroup_aws_account" {
-  type    = number
-  default = "753095100886"
+variable "aws_master_billing_account" {
+  type        = number
+  description = "AWS account number where billing is consolidated."
 }
+
 variable "customer_name_slug" {
   type        = string
   description = "A short, lower-case slug that identifies your company, e.g. 'acme-corp'. Duckbill Group will need to know this value, so that we can set up our own infrastructure for you."
 }
 
-variable "cur_bucket_name" {
-  type        = string
-  description = "Name of the S3 bucket in which you are storing Cost and Usage Reports."
-}
-
 variable "external_id" {
   type        = string
   description = "Customer Specific External ID string"
+}
+
+variable "cur_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket in which you are storing Cost and Usage Reports."
 }
 
 variable "cur_bucket_name_create" {
@@ -40,7 +41,7 @@ variable "duckbillgroup_cur_report_create" {
   default     = false
 }
 
-variable "aws_master_billing_account" {
-  type        = number
-  description = "AWS account number where billing is consolidated."
+variable "duckbillgroup_aws_account" {
+  type    = number
+  default = "753095100886"
 }
